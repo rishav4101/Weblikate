a2ba43b3d16497fa9019064a62f01061591bf66b
rishav4101 <rajkumarrishav4101@gmail.com>
2020-07-26 02:21:34 +0530

    hostfix3

--- components/Layout.js
+++ components/Layout.js
@@ -12,36 +12,35 @@ const Layout = (props) => {
     if (!vantaEffect) {
       setVantaEffect(
         WAVES({
-          el: myRef.current,
+          el: "#van",
           THREE: THREE,
           mouseControls: true,
           touchControls: true,
           minHeight: 200.0,
           minWidth: 200.0,
           scale: 1.0,
-          scaleMobile: 1.0,
+          scaleMobile: 1,
           color: 0x0,
           shininess: 32.0,
           waveHeight: 23.5,
           waveSpeed: 0.6,
-          zoom: 0.86,
+          zoom: 0.8,
         })
       );
     }
   }, [vantaEffect]);
   return (
     <div>
-      <div ref={myRef} className="container">
-        <Head>
-          <title>Zaffre</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <body>
-          <Mainnav />
-          {props.children}
-          <footer>copyright&copy; 2020 Zaffre</footer>
-        </body>
-      </div>
+      <div ref={myRef} className="container" id="van"></div>
+      <Head>
+        <title>Zaffre</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Mainnav />
+        {props.children}
+        <footer>copyright&copy; 2020 Zaffre</footer>
+      </body>
     </div>
   );
 };
